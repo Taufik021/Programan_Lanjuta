d067b11acdd18aa1012b9b54d156c161c648c544
Taufik021 <tugastaufik1726@gmail.com>
2025-10-23 21:52:18 +0700

    Baru Taufik

---
+++ src/ppickk.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ppickk {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("===----------===");
+        System.out.println("======----======");
+        System.out.print("Masukkan Nama: ");
+        String nama = input.nextLine();
+        // System.out.print("APA YANG TERJADI DI DUNIA INI: ");
+        //  String dunia = input.nextLine();
+        System.out.print("KASI KATA-KATA HARI");
+        //  String kata = input.nextLine();
+
+        System.out.println("\nSEMANGAT " + nama + "");
+        System.out.println("JADIKANLAH UJIAN DI DUNIA ITU MENJADI TEMAN SEMENTAR KAMU");
+    }
+}
